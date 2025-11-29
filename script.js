8af549640ad36716a0833c6bcfc01c7411c0f0f2
dianahshimwa <d.shimwa@alustudent.com>
2025-11-29 21:05:43 +0200

    Local storage update but not done yet

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const statusText = document.getElementById("status");
 const list = document.getElementById('list');
 const reciresult = document.getElementById('results')
 
+window.addEventListener('load', () => {
+    const Savedreci = JSON.parse(localStorage.getItem('recipes')) || [];
+    Savedreci.forEach(recipes => {
+        addrecipe(recipes);
+    });
+});
+
+function saveRecipeToLocalStorage(recipes){
+    const reci = JSON.parse(localStorage.getItem('recipes')) || [];
+    reci.push(recipes);
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+}
+
 function addrecipe(){
     let li = document.createElement("li");
     if(input.value === ''){
